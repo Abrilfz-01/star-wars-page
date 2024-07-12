cdabc687246dee484db618a07e83c161396ce3f1
Abril <abril.nahuel@hotmail.com>
2024-07-12 00:45:24 -0300

    Realice cambios en el JS

--- js/app.js
+++ js/app.js
@@ -168,36 +168,3 @@ document.addEventListener('keyup',e =>{
         }
     }
 })
-
-
-
-
-
-
-
-
-
-/*
-for (let i = 1; i <=81; i++){
-    fetch(`${URLBase}${endpointPersonajes}` + i)
-    .then(response=>response.json()
-    .then(data=>createCards(data.results))
-    .catch(error=>{
-        console.error('Error al obtener los personajes', error)
-    })
-)
-}
-
-
-function createCards(data){
-    const div = document.createElement('div');
-    div.classList.add("personaje");
-    div.innerHTML = `<div class="card-p">
-    <h1>Luke Skywalker</h1>
-    <p>Altura: 172cm</p>
-    <p>Peso: 77kg</p>
-    <p>Genero: Masculino</p>
-    <a href="#">Leer mas</a>
-    </div>`;
-    listaPersonaje.append(div);
-}*/
